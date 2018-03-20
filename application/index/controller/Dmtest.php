96abe1c9bdd2dea861ebaa46ebfd5c9970e37887
jinshuai <89460766@qq.com>
2018-03-20 18:14:57 +0800

    branch test two

---
+++ application/index/controller/Dmtest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: js
+ * Date: 2018/3/14
+ * Time: 17:46
+ */
+namespace app\index\controller;
+use think\Controller;
+class Dmtest extends Controller
+{
+    public function index(){
+
+        return view('dmtest');
+    }
+
+}
